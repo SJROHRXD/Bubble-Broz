6bfbf0fd48f3d0d24e157a7ddcce13a3275106ec
SJRXD <rohr.olasz@gmail.com>
2023-04-24 04:35:34 -0700

    COMPONENTS: adding basic components, JS, to fill out app structure

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+export function Header() {
+    // add header elements and logic here
+
+
+    return header;
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-// here
+import './style.css';
+import { Header } from './components/Header.js';
+import { Card } from './components/Card.js';
+
+const app = document.querySelector('#app');
+
+// Create and add header
+const header = Header();
+app.appendChild(header);
+
+// Create and add card
+const card = Card();
+app.appendChild(card);
